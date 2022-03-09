65fd9ad4f3cd23ec9702b183b8c62bae30db299e
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-03-09 18:55:29 +0100

    Update ejercicio7.asm

--- sesion3/ejercicio7.asm
+++ sesion3/ejercicio7.asm
@@ -18,82 +18,93 @@ suma2: .byte 0
 temp: .word 0
 
 	.globl programa
+
+num1:	.byte 0
+num2:	.byte 0
+num3:	.byte 0
+num4:	.byte 0
+temp:	.byte 0
+
 programa:
-	ldb teclado
-	stb num1
-	ldb teclado
-	stb num2
-	
-	ldb #0x20
-	stb pantalla
+				lda teclado
+				sta num1
+				lda teclado
+				sta num2
+				lda #' 
+				sta pantalla
+				
+				lda teclado
+				sta num3
+				lda teclado
+				sta num4
+				sta pantalla
+				
+				lda #'\n
+				sta pantalla
+				
+				lda num1
+				sta pantalla
+				
+				lda num2
+				sta pantalla
+				
+				lda #'+
+				sta pantalla
 
-	ldb teclado
-	stb num3
-	ldb teclado
-	stb num4
-
-	ldb #0x20
-	stb pantalla
-
-	; Suma
-	lda num2
-	adda num4
-	sta suma2
-	lda num1
-	adca num3
-	sta suma1
-
-	; Salto de línea
-	ldb #'\n
-	stb pantalla
-
-	; Resultado
-	ldb num1
-	stb pantalla
-	ldb num2
-	stb pantalla
-	ldb #0x2B	; Simbolo '+'
-	stb pantalla
-	ldb num3
-	stb pantalla
-	ldb num4
-	stb pantalla
-	ldb #0x3D	; Simbolo '='
-	stb pantalla
-
-	; Aritmética BCD
-	lda suma1
-	tfr a,b
-	lsrb
-	lsrb
-	lsrb
-	lsrb
-	std temp
-	clra
-	addb #246
-	adca #0
-	ldb #'A-'9-1
-	mul
-	addb temp+1
-	addb #'0
-	stb pantalla
-	
-	lda suma2
-	tfr a,b
-	lsrb
-	lsrb
-	lsrb
-	lsrb
-	lsrb
-	std temp
-	clra
-	addb #246
-	adca #0
-	ldb #'A-'9-1
-	mul
-	addb temp+1
-	addb #'0
-	stb pantalla
+				lda num3
+				sta pantalla
+				
+				lda num4
+				sta pantalla
+				
+				lda #'=
+				sta pantalla
+				
+				lda num2
+				suba #'0
+				ldb num4
+				subb #'0
+				stb  num4
+				adda num4
+				daa
+				sta temp
+				
+				lda num1
+				suba #'0
+				ldb num3
+				subb #'0
+				stb num3
+				adda num3
+				
+				lsla
+				lsla
+				lsla
+				lsla
+				adda temp
+				sta temp
+				
+				tfr a,b
+				lsrb
+				lsrb
+				lsrb
+				lsrb ; en B esta la primera cifra
+				addb #'0
+				stb pantalla
+				
+				lda temp
+				tfr a,b
+				lsrb
+				lsrb
+				lsrb
+				lsrb
+				lsrb
+				lsrb
+				lsrb
+				lsrb
+				
+				addb #'0
+				stb pantalla
+				
 
 	; Salto de linea
 	ldb #'\n
